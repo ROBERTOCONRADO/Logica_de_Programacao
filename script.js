4d785f0e2f0ceff5e0c21d8718bc0e9632d37809
RobertoConrado <robertoconradorc@gmail.com>
2022-07-09 15:03:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+//Exercicios separados //
+
+var numero = prompt("Digíte um número ");
+numero = parseFloat(numero);
+var numero2 = prompt("Digíte outro número");
+numero2 = parseFloat(numero2);
+subtracao = (numero - numero2);
+divisao = (numero / numero2);
+soma = (numero + numero2);
+alert("A subtração de " + numero + " - " + numero2 + " é: " + subtracao);
+alert("A multiplicação é " + (numero * numero2));
+alert("A divisão é " + divisao + " E a soma é " +soma);
+
+//===========================================================
+
+var n1 = prompt("Digite um número");
+n1 = parseInt(n1);
+var n2 = prompt("Digite outro número")
+n2 = parseInt(n2);
+var n3 = prompt("Digite um número");
+n1 = parseInt(n3);
+var n4 = prompt("Digite outro número")
+n2 = parseInt(n4);
+var n5 = prompt("Digite um número");
+n1 = parseInt(n5);
+var n6 = prompt("Digite outro número")
+n2 = parseInt(n6);
+var n7 = prompt("Digite um número");
+n1 = parseInt(n7);
+var n8 = prompt("Digite outro número")
+n2 = parseInt(n8);
+var media =  (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8) / 2;
+alert(media);        
+
+//===========================================================
+
+let numero = prompt("digite um numero");
+numero = parseInt(numero);
+let numero2 = prompt("digite outro numero");
+numero2 = parseInt(numero2);
+alert("o resto da divisão de " + numero + " por " + numero2 + " é: " + (numero % numero2));
+alert("O numero " + numero + " elevado a " + numero2 + " é: " + (numero ** numero2));
+
+//===========================================================
+
+let numero1 = prompt("Digite um número");
+let numero2 = parseFloat(prompt("Digite outro número"));// forma abreviada//
+numero1 = parseFloat(numero1);
+let media = (numero1 + numero2) / 2 ;
+alert(media > 5);// vai retornar true ou false //
+
+//===========================================================
+
+let n1 = parseFloat(prompt("Digite um número"));
+let n2 = parseFloat(prompt("Digite outro número"));
+let media = (n1 + n2) / 2;
+if (media >= 5 ) {
+    alert("Aprovado!");
+} else {
+    alert("Reprovado!");
+}
+
+//===========================================================
+
+let jogador1 = parseInt(prompt("Jogador 1 Digite um número"));//Fazer jogador1 digitar um número
+let jogador2 = parseInt(prompt("Jogador 2 Digite um número"));//Fazer jogador2 digitar um número
+
+if (jogador1 === jogador2) {
+    alert("Ouve um empate!");
+//Se forem iguais mostrar mensagem 'empate' e encerrar o programa
+} else {
+        let numeroSorteado = parseInt(Math.random() * 2);
+        //Se forem diferentes, sortear um número entre 0 e 1
+    
+    if (numeroSorteado === 0) {//Se o número sorteado for 0, ganha quem escolher o número MENOR
+        if (jogador1 < jogador2) {
+            alert("Jogador 1 é o Vencedor!")
+        } else {
+            alert("Joagador 2 é o Vencedor!");
+        }
+    } else {//senão, ganha quem escolher o número MAIOR.
+        if (jogador1 > jogador2) {
+            alert("Jogador 1 é o Vencedor!")
+        } else {
+            alert("Jogador 2 é o Vencedor!");
+        }
+    }
+}
+
+//===========================================================
+
+let nota1 = parseFloat(prompt("Digite um número"));
+let nota2 = parseFloat(prompt("Digite um número"));
+let media = (nota1 + nota2) / 2;
+if (nota1 > 0 && nota2 > 0 && media > 5) {
+    alert("Parabéns, você foi aprovado!")
+} else {
+    alert("Reprovado! Estude um pouco mais.")
+}
